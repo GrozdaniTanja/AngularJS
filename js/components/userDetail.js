80cd0e6237398e26c0a3d3bef2fb7acdf1d21a17
Tanja <tanja.grozdani@student.um.si>
2024-11-24 18:04:03 +0100

    fix transclude- reusability

--- js/components/userDetail.js
+++ js/components/userDetail.js
@@ -1,32 +1,19 @@
-
-angular.module('employeeApp').component('userDetail', {
+angular.module('employeeApp').component('popup', {
     transclude: true,
     bindings: { 
-        employee: '<', 
-        onDelete: '&'        
+        title: '@', 
+        onClose: '&' 
     },
     template: `
-        <div class="modal show d-block" ng-if="$ctrl.employee">
+        <div class="modal show d-block">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header bg-primary text-white">
-                        <h4 class="modal-title">User Details</h4>
-                        <button type="button" class="btn-close" ng-click="$ctrl.employee = null">&times;</button>
+                        <h4 class="modal-title">{{$ctrl.title}}</h4>
+                        <button type="button" class="btn-close" ng-click="$ctrl.onClose()">&times;</button>
                     </div>
                     <div class="modal-body">
-                        <p><strong>Username:</strong> {{$ctrl.employee.username}}</p>
-                        <p><strong>Name:</strong> {{$ctrl.employee.name}}</p>
-                        <p><strong>Email:</strong> {{$ctrl.employee.email}}</p>
-                        <p><strong>Website:</strong> {{$ctrl.employee.website}}</p>
-                        <p><strong>Phone:</strong> {{$ctrl.employee.phone}}</p>
-                        <p><strong>Address:</strong> 
-                            {{$ctrl.employee.address.street}}, 
-                            {{$ctrl.employee.address.city}}
-                        </p>
                         <div ng-transclude></div>
-                    </div>
-                    <div class="modal-footer">
-                        <button class="btn btn-danger" ng-click="$ctrl.onDelete()">Delete</button>
                     </div>
                 </div>
             </div>
