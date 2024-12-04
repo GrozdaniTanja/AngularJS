7a7f02113748aca4905e6a2ce81145d629d8bbbc
Tanja <tanja.grozdani@student.um.si>
2024-12-04 14:04:01 +0100

    fix longpress

--- js/directives/onLongPress.js
+++ js/directives/onLongPress.js
@@ -1,16 +1,31 @@
 angular.module('employeeApp').directive('onLongPress', ['$timeout', function($timeout) {
     return {
-        restrict: 'A',
+        restrict: 'A', 
         link: function(scope, element) {
             let timer;
+            const LONG_PRESS_DELAY = 1000;  
 
             element.on('mousedown', function() {
                 timer = $timeout(function() {
                     console.log('Long press detected!');
-                }, 1000);
+                }, LONG_PRESS_DELAY);
             });
 
             element.on('mouseup mouseleave', function() {
+                $timeout.cancel(timer);
+            });
+
+            element.on('touchstart', function() {
+                timer = $timeout(function() {
+                    console.log('Long press detected!');
+                }, LONG_PRESS_DELAY);
+            });
+
+            element.on('touchend touchcancel', function() {
+                $timeout.cancel(timer);
+            });
+
+            element.on('$destroy', function() {
                 $timeout.cancel(timer);
             });
         }
